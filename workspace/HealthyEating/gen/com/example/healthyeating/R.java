da88ade552349f017af8e2528084c188f8765936
Gregory Ferguson <gf60459n@pace.edu>
2014-04-04 11:24:07 -0700

    added recipe layout

--- workspace/HealthyEating/gen/com/example/healthyeating/R.java
+++ workspace/HealthyEating/gen/com/example/healthyeating/R.java
@@ -25,24 +25,41 @@ public final class R {
         public static final int three_stars=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f08000c;
+        public static final int ImageView01=0x7f080013;
+        public static final int ImageView02=0x7f080012;
+        public static final int ImageView03=0x7f080014;
+        public static final int action_settings=0x7f08001b;
         public static final int dishDiscription=0x7f08000a;
         public static final int dishImage=0x7f080007;
         public static final int dishName=0x7f080008;
         public static final int dishRating=0x7f08000b;
         public static final int dishServes=0x7f080009;
         public static final int homeHealthFacts=0x7f080002;
+        public static final int imageView1=0x7f08000c;
+        public static final int imageView2=0x7f080011;
         public static final int tab1=0x7f080001;
         public static final int tab2=0x7f080005;
         public static final int tab3=0x7f080006;
         public static final int tabhost=0x7f080000;
+        public static final int textView1=0x7f08000f;
+        public static final int textView10=0x7f08001a;
+        public static final int textView2=0x7f08000e;
+        public static final int textView3=0x7f08000d;
+        public static final int textView4=0x7f080010;
+        public static final int textView5=0x7f080015;
+        public static final int textView6=0x7f080016;
+        public static final int textView7=0x7f080017;
+        public static final int textView8=0x7f080018;
+        public static final int textView9=0x7f080019;
         public static final int top20=0x7f080004;
         public static final int top5Recipes=0x7f080003;
     }
     public static final class layout {
-        public static final int home_page=0x7f030000;
-        public static final int image_list_item=0x7f030001;
-        public static final int splash=0x7f030002;
+        public static final int activity_main=0x7f030000;
+        public static final int home_page=0x7f030001;
+        public static final int image_list_item=0x7f030002;
+        public static final int recipe=0x7f030003;
+        public static final int splash=0x7f030004;
     }
     public static final class menu {
         public static final int main=0x7f070000;
