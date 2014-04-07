7fca8620be16914b4ac4ff67ded1a72608127772
John Goodwin <goodwinj14@yahoo.com>
2014-04-07 15:26:39 -0400

    local

--- workspace/HealthyEating/gen/com/example/healthyeating/R.java
+++ workspace/HealthyEating/gen/com/example/healthyeating/R.java
@@ -63,6 +63,7 @@ public final class R {
         public static final int image_list_item=0x7f030001;
         public static final int recipe=0x7f030002;
         public static final int splash=0x7f030003;
+        public static final int testgit=0x7f030004;
     }
     public static final class menu {
         public static final int main=0x7f070000;
