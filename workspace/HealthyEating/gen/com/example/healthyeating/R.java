68b6b7e34f30e30afaecca96350f5a8f2b0bb829
John Goodwin <goodwinj14@yahoo.com>
2014-04-21 22:54:10 -0400

    apk

--- workspace/HealthyEating/gen/com/example/healthyeating/R.java
+++ workspace/HealthyEating/gen/com/example/healthyeating/R.java
@@ -81,11 +81,10 @@ public final class R {
         public static final int topRatedRecipesB=0x7f080009;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
-        public static final int home_page=0x7f030001;
-        public static final int image_list_item=0x7f030002;
-        public static final int recipe=0x7f030003;
-        public static final int splash=0x7f030004;
+        public static final int home_page=0x7f030000;
+        public static final int image_list_item=0x7f030001;
+        public static final int recipe=0x7f030002;
+        public static final int splash=0x7f030003;
     }
     public static final class menu {
         public static final int main=0x7f070000;
